121814b27a78c1ab3d2cb26c999d6595e1d09500
Gabriel Matute <gmatute@berkeley.edu>
2020-11-22 19:51:03 -0500

    Make lists into nodes.

--- gumtree/python.py
+++ gumtree/python.py
@@ -1,41 +1,55 @@
-from ast import AST, iter_child_nodes
-from typing import Any, Iterable, Optional
+from ast import AST, iter_fields
+from typing import Any, Iterable, Optional, Union, get_args
 
 from .adapter import BaseAdapter, memoize
 
 # https://github.com/PyCQA/pylint/issues/3882
 # pylint: disable=unsubscriptable-object
 
+from ast import expr_context, boolop, operator, unaryop, cmpop
+Enums = (expr_context, boolop, operator, unaryop, cmpop)
 
-class Adapter(BaseAdapter[AST]):
-    def __init__(self, root1: AST, root2: AST):
+
+Node = Union[AST, list]
+
+
+class Adapter(BaseAdapter[Node]):
+    def __init__(self, root1: Node, root2: Node):
         self._parents = {}
         for root in [root1, root2]:
             self._update_parents(root)
 
-    def parent(self, n: AST) -> Optional[AST]:
+    def parent(self, n: Node) -> Optional[Node]:
         return self._parents.get(id(n), None)
 
     @staticmethod
-    def children(n: AST) -> Iterable[AST]:
-        return iter_child_nodes(n)
+    def children(n: Node) -> Iterable[Node]:
+        if isinstance(n, AST):
+            it = iter_fields(n)
+        if isinstance(n, list):
+            it = enumerate(n)
+
+        for _, value in it:
+            if isinstance(value, (AST, list)) and not isinstance(value, Enums):
+                yield value
 
     @staticmethod
-    def label(n: AST) -> str:
+    def label(n: Node) -> str:
         return type(n).__name__
 
     @memoize
-    def value(self, n: AST) -> Any:
+    def value(self, n: Node) -> Any:
         terminals = []
-        for attr in dir(n):
-            if attr.startswith('_') \
-                    or 'lineno' in attr or 'col' in attr \
-                    or isinstance(v := getattr(n, attr, None), (AST, list)):
-                continue  # Skip metadata and AST nodes
-            terminals.append((attr, v))
+        if isinstance(n, AST):
+            for name, value in iter_fields(n):
+                if isinstance(value, list) and value and all(isinstance(e, Enums) for e in value):
+                    terminals.append((name, value))
+                if value is not None and not isinstance(value, (AST, list)) or isinstance(value, Enums):
+                    terminals.append((name, value))
         return terminals
 
     def _update_parents(self, root):
         for child in self.children(root):
+            assert id(child) not in self._parents
             self._parents[id(child)] = root
             self._update_parents(child)
